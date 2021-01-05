6e0035b1a4de4ef85dffe609e23d2d23ef9987eb
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-05 23:22:57 +0500

    added template for owners/ url. changed url of owners. closes #15

--- pet-clinic-web/src/main/java/com/example/demo/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/demo/controllers/OwnerController.java
@@ -17,7 +17,7 @@ public class OwnerController {
     }
 
 
-    @RequestMapping({"/","/index","index.html"})
+    @RequestMapping({"", "/","/index","index.html"})
     public String OwnerList(Model model){
         model.addAttribute("owners",ownerService.findAll());
 
