68969cfdf5d4b3f0b8208822c34ce37b5c1b518c
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-06 23:56:05 +0500

    added PetServiceMap. Closes #20

---
+++ pet-clinic-data/src/main/java/com/example/demo/service/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service.map;
+
+import com.example.demo.model.PetType;
+import com.example.demo.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstracMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
