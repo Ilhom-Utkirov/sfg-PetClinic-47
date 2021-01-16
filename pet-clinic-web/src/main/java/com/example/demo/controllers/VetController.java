c8e748aafb5c0d90f41ac159d0b7df5013e2dea4
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-17 01:58:52 +0500

    fixed bugs in the menu link . closes #34

--- pet-clinic-web/src/main/java/com/example/demo/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/demo/controllers/VetController.java
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vet", "/vet/index", "vet/index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String vetList(Model model){
         model.addAttribute("vets", vetService.findAll());
 
