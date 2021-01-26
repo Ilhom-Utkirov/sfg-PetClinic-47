6f546bcea8cb9b2dc27bf00a1fedf6210f126ad5
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-26 23:23:57 +0500

    added Jpa classes to the .closes #25

--- pet-clinic-data/src/main/java/com/example/demo/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/Pet.java
@@ -2,6 +2,8 @@ package com.example.demo.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +23,8 @@ public class Pet extends BaseEntity{
     @Column(name = "birthDate")
     private LocalDate birthDate;
 
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
