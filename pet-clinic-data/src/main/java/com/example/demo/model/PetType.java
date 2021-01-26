64f5fdf8152c7ce81da050940d79884ca84d01b7
O'tkirov Ilhom <inspiration95@mail.ru>
2021-01-26 23:14:11 +0500

    added Jpa classes to the .closes #25

--- pet-clinic-data/src/main/java/com/example/demo/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/PetType.java
@@ -2,7 +2,11 @@ package com.example.demo.model;
 
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
     @Column(name ="name")
--- pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/demo/model/Vet.java
@@ -1,12 +1,19 @@
 package com.example.demo.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
-
-
+    // it is Lazy by default but we want database to be loaded at once
+    //specialities would be null if donot do eager
+@ManyToMany(fetch = FetchType.EAGER)
+@JoinTable(name = "vet_specialities",
+        joinColumns = @JoinColumn(name = "vet_id"),
+        inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 private Set<Speciality> specialities = new HashSet<>() ;
 
     public Set<Speciality> getSpecialities() {
